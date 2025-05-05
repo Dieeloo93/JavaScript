43d9b8902a8d6cbf9995b79fff70d6ceeca85ec1
Diego Lopez <lopezdiegog1993@gmail.com>
2025-05-05 19:10:28 -0300

    cambios

--- main.js
+++ main.js
@@ -1,37 +1,104 @@
-let seleccion = prompt("Ingrese la opcion del producto que desea llevar, para salir ingrese 0")
-let seleccionarProductos = Number(prompt( "1-Buzo $3000 2-Remera $1500 3-Jean $5000 4-Campera $6000 "))
-let seleccionarCantidad;
-let total = 0;
+const contenidocompra = document. getElementById("contenidocompra") 
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
 
+const productos = [
+    {
+      id: 1,
+      nombre: "Buzo",
+      precio: 5000,
+            cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Remera",
+      precio: 1000,
+     cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Jean",
+      precio: 1500,
+    cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Zapatilla",
+      precio: 2000,
+      cantidad: 1,
+    },
+  ];
 
-const cantidad = (cant, precio) => {
-  return cant * precio
-}
+  const productosJSON = JSON.stringify(productos)
+  localStorage.setItem("productos",productosJSON)
+  console.log (localStorage.getItem("productos"))
 
+  const productos1 = JSON.parse(localStorage.getItem("productos"))
+  console.log(productos1)
 
-while (seleccionarProductos != 0) {
-switch (seleccionarProductos) {
-    case 1:
-seleccionarCantidad= Number(prompt("el producto seleccionado es Buzo, indique la cantidad"))
-            total += cantidad(seleccionarCantidad, 3000)
-break;
-case 2:
-        seleccionarCantidad = Number(prompt("el producto seleccionado es Remera, indique la cantidad"))
-        total += cantidad(seleccionarCantidad, 1500)
-break;
-    case 3:
-seleccionarCantidad = Number(prompt("el producto seleccionado es Jean, indique la cantidad"))
-total += cantidad(seleccionarCantidad, 5000)
-    break;
-    case 4:
-seleccionarCantidad = Number(prompt("el producto seleccionado es Campera, indique la cantidad"))
-total += cantidad(seleccionarCantidad, 6000)
-break;
+  let carrito = [];
 
-default:
-break;
-}
-seleccionarProductos = Number(prompt( "1-Buzo $3000 2-Remera $1500 3-Jean $5000 4-Campera $6000 "))
-}
+  productos.forEach ((product)=>{
+    let content = document.createElement ("div");
+    content.className = "card";
+    content.innerHTML = `
+    <h3>${product.nombre}</h3>
+    <p class="price">${product.precio} $</p>
+    `;
+contenidocompra.append (content);
 
-alert("el total de la compra es de: " + total)
+let comprar = document.createElement("button");
+comprar.innerText = "Comprar";
+comprar.className = "comprar";
+
+content.append (comprar);
+
+comprar.addEventListener("click", ()=> {
+    carrito.push({
+     id : product.id,
+     nombre: product.nombre,
+     precio: product.precio,
+    });
+
+    console.log (carrito);
+});
+  });
+
+  verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement ("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modal- header-title">Carrito.</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click",() => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+carrito.forEach((product) => { 
+    let carritoContect = document.createElement("div");
+    carritoContect.className = "modal-content";
+    carritoContect.innerHTML = `
+    <h3>${product.nombre}</h3>
+    <p>${product.precio} $</p>
+    `;
+    modalContainer.append(carritoContect);
+  });
+
+  const total = carrito.reduce ((acc,el)=> acc + el.precio,0);
+
+  const totalbuying = document.createElement ("div");
+  totalbuying.className = "total-content";
+  totalbuying.innerHTML = `total a pagar: ${total} $`;
+  modalContainer.append (totalbuying);
+});
+
